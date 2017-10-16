729e8da656cec4dfc009809291d9783db8c621eb
Dragos G <dragosgn@gmail.com>
2017-10-16 10:09:30 +0200

    styled clacl

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,15 +6,39 @@ import {Field} from 'redux-form'
 
 
 const Root = styled.div`
-  padding: 30px;
+  padding: 4rem;
+`
+
+const StyledField = styled(Field)`
+  margin: 1rem;
+  padding: 0.5rem
+`
+
+const StyledForm = styled.form`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const StyledButton = styled.button`
+  max-width: 5rem;
+  padding: 0.5rem;
+  border-radius: 5px;
 `
 
 let Calculator = (props) => {
   return <Root>
-    <form>
-      <Field {...props} component="input" type="text" name="netIncome"/>
-      <button type="submit">Submit</button>
-    </form>
+    <StyledForm>
+      <StyledField {...props} component="input" type="text" name="netIncome"/>
+      <StyledField {...props} component="input" type="text" name="rent"/>
+      <StyledField {...props} component="input" type="text" name="transportation"/>
+      <StyledField {...props} component="input" type="text" name="food"/>
+      <StyledField {...props} component="input" type="text" name="phone"/>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+    <div>
+    </div>
   </Root>
 }
 
