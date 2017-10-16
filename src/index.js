886e8b8f471448a84578aaf1a277e29cbcf89f6c
Dragos G <dragosgn@gmail.com>
2017-10-16 09:39:25 +0200

    improved linter, redux forms

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-import { createStore, combineReducers } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {ThemeProvider} from 'styled-components'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
