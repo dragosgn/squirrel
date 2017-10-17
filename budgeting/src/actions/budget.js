8b6efbe2e941d68cb9926942dde1f6aa1e59b13e
Dragos G <dragosgn@gmail.com>
2017-10-17 17:56:02 +0200

    corrected linter

--- budgeting/src/actions/budget.js
+++ budgeting/src/actions/budget.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default function() {
-  const url = "http://localhost:2000/"
+  const url = "http://localhost:2000/budget"
   const request = axios.get(url)
   return {
     type: "FETCH_BUDGET",
