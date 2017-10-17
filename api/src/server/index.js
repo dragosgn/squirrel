206c003eb23f0c2aeae3eeeeaeada319a1c3949a
Dragos G <dragosgn@gmail.com>
2017-10-17 10:18:58 +0200

    worked on the api

--- api/src/server/index.js
+++ api/src/server/index.js
@@ -1,13 +1,34 @@
-import express from 'express'
-import https from 'https'
-import http from 'http'
-import fs from 'fs'
-import path from 'path'
-import reloadable from "express-reloadable"
-import setup from "./setup"
+let express = require("express")
+let bodyParser = require("body-parser")
+let morgan = require("morgan")
+let pg = require('pg')
+const PORT = 2000
 
 
-const app = express()
+let pool = new pg.Pool({
+  port: 5500,
+  password: "",
+  database: "",
+  
+})
 
+var app = express()
 
-if(process.env.NODE_ENV)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(morgan('dev'))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.get('/', function(req, res) {
+  res.send('Hello world!')
+})
+
+app.get('/budget', function(request, response) {
+
+})
