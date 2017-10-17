2f31e951a2889ffd68f7229d5da566e2249138e7
Dragos G <dragosgn@gmail.com>
2017-10-17 15:49:23 +0200

    added the thunk

--- budgeting/src/index.js
+++ budgeting/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {ThemeProvider} from 'styled-components'
-import { createStore, combineReducers } from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import {Provider} from "react-redux"
 import {render} from 'react-dom'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import {
-  BrowserRouter as Router
-} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
+import thunk from "redux-thunk"
+
 
 import budgetReducer from "./reducers/budget"
 
@@ -27,7 +27,9 @@ const theme = {
   brandGrey: "#586065"
 }
 
-let store = createStore(rootReducer, composeWithDevTools())
+let store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk)
+))
 
 const renderApp = (Component) => {
   render(
