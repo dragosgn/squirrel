b97f3879668c6adf38b0326dca6d394a58850a7e
Dragos G <dragosgn@gmail.com>
2017-10-18 11:51:26 +0200

    small front end changes

--- budgeting/src/components/Calculator.js
+++ budgeting/src/components/Calculator.js
@@ -10,29 +10,43 @@ const Root = styled.div`
   padding: 4rem;
   display: flex;
   flex-direction: column;
-  max-width: 300px;
+  max-width: 400px;
 `
 
 const Row = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 0.5rem;
+  padding: 0.5rem;
 `
 
 const Button = styled.button`
   background-color: ${ props => props.brandBlue};
   padding: 0.5rem;
+  margin-top: 1rem;
 `
 
 const MoneyInput = styled(Field)`
   padding: 0.25rem;
 `
 
+const StyledForm = styled.form`
+  border: 1px solid grey;
+  border-radius: 2px;
+  padding: 1rem;
+`
+
+const ButtonRow =styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`
+
 
 let Calculator = (props) => {
   return <Root>
-        <form>
+        <StyledForm>
             <Row>
               <label>Net Income (€)</label>
               <MoneyInput component="input" type="text" name="netIncome"/>
@@ -53,10 +67,10 @@ let Calculator = (props) => {
               <label >Phone (€)</label>
               <MoneyInput component="input" type="text" name="phone"/>
             </Row>
-            <Row style={{ justifyContent: "flexEnd"}}>
+            <ButtonRow>
               <Button type="submit">Submit</Button>
-            </Row>
-          </form>
+            </ButtonRow>
+          </StyledForm>
           <Row>
             <p>Total monthly spendings:</p>
             <h5>{props.spendings} (€)</h5>
