e07a83b9d25ad316320edee9a8e3373850003aa2
Dragos G <dragosgn@gmail.com>
2017-11-02 17:47:34 +0100

    creating investment component

---
+++ budgeting-app/src/components/InvestmentCalc.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import {compose} from 'recompose'
+
+
+const Root = styled.div`
+
+`
+
+let InvestmenCalc = () => (
+  <Root>
+    Investment calc
+  </Root>
+)
+
+
+export default compose()(InvestmenCalc)
