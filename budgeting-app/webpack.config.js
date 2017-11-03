fa5efdcaeef0db41aa4504551dad5f0bcc41f8fd
Dragos G <dragosgn@gmail.com>
2017-11-03 15:55:37 +0100

    webpack config

---
+++ budgeting-app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader'}
+    ]
+  }
+}
+
+module.exports = config;
