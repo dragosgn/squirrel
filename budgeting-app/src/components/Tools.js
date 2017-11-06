5dc65e4b94aa524dc8c938929919d8d483e6e49a
Dragos G <dragosgn@gmail.com>
2017-11-06 09:55:41 +0100

    new tools page

---
+++ budgeting-app/src/components/Tools.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import {compose} from 'recompose'
+import {Link} from 'react-router-dom'
+
+
+
+const Root = styled.div`
+
+`
+
+const MenuBox = styled(Link)`
+  padding: 1rem;
+  border: 1px solid grey;
+  border-radius: 5px;
+`
+
+const Tools = () => (
+  <Root>
+  </Root>
+)
